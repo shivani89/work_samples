b4ec462d6d4f8666f6af7e8d127c39d16614a763
shivani89 <shivani.ui89@gmail.com>
2016-01-22 12:55:52 -0800

    Animating app using Angular

---
+++ app.js
@@ -0,0 +1,10 @@
+angular.module('animateApp', ['ngAnimate'])
+
+.controller('mainController', function($scope) {
+  
+  // set the default states for lions and cranes
+  $scope.lions = false;
+  $scope.cranes = false;
+});
+
+         
